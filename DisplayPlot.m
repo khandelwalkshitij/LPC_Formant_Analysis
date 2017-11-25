d3428e61293117929a5ed874d7ef1e10316a78eb
khandelwalkshitij <kshitij14159@gmail.com>
2017-11-25 18:26:24 +0530

    Added Code For Displaying The Plots

--- DisplayPlot.m
+++ DisplayPlot.m
@@ -1,36 +1,6 @@
 close all;
 clear all;
-
-%SSN Generation
-%*********************
-% Fs=10000;
-% t = 1/Fs:1/Fs:3200/(Fs);
-% x = sin(2*pi*2000*t); % Create sawtooth signal.
-% y = awgn(x,10,'measured'); % Add white Gaussian noise.
-% y=y-x;
-% % subplot(2,1,1);
-% Y=fftshift(fft(y));
-% l=length(y);
-% f=-l/2:l/2-1;
-% % plot(f*Fs/l,abs(Y));
-% xlim([-8000 8000]);
-% rp = 0.8;           % Passband ripple
-% rs = 0.01;          % Stopband ripple
-% f = [4000 4250];    % Cutoff frequencies
-% a = [1 0];        % Desired amplitudes
-% dev = [0.08 0.1];
-% [n,fo,ao,w] = firpmord(f,a,dev,Fs);
-% % subplot(2,1,2);
-% b1 = firpm(n,fo,ao,w);
-% y1=filter(b1,1,y);
-% noise=y1';
-% % subplot(2,1,2);
-% Y1=fftshift(fft(y1));
-% l1=length(y1);
-% f1=-l1/2:l1/2-1;
-% plot(f1*Fs/l1,abs(Y1));
-% xlim([-8000 8000]);
-
+=
 [noise Fs]=audioread('sachanisrichasfuck.wav');
 noise=noise(4000:7999);
 %*********************
